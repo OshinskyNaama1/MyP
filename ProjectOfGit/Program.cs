8ebff22180aec1dd540978289f30711e97f970ca
Naama Oshinsky <naamaoshi@gmail.com>
2020-10-12 16:37:49 +0300

    init commit

--- ProjectOfGit/Program.cs
+++ ProjectOfGit/Program.cs
@@ -7,6 +7,8 @@ namespace ProjectOfGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hiiiiiiiiiii!!!!!!!!!");
+            Console.WriteLine("lll");
         }
     }
 }
